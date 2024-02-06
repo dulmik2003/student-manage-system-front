99c02f80c31c210b32e41475d3c4034821c87b24
Dulmika <semaldulmika@gmail.com>
2024-02-06 18:23:31 +0530

    main structure and routing flow done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "registration",
+    component: RegistrationComponent
+  },
+  {
+    path: "student-list",
+    component: StudentListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { SideNavComponent } from './common/side-nav/side-nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    HomeComponent,
+    RegistrationComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
