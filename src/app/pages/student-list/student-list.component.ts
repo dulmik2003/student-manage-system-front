c0742cf3b2e6dd8cbe6c503a550f7cea69713cbb
Dulmika <semaldulmika@gmail.com>
2024-02-07 15:13:02 +0530

    view student list

--- src/app/pages/student-list/student-list.component.ts
+++ src/app/pages/student-list/student-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
 })
-export class StudentListComponent {
+export class StudentListComponent implements OnInit {
+  private http;
+  public studentList: any;
 
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadStudent();
+  }
+
+  loadStudent() {
+    this.http.get("http://localhost:8080/student/get-all")
+      .subscribe((data) => {
+        this.studentList = data;
+        console.log(this.studentList);
+      });
+  }
 }
